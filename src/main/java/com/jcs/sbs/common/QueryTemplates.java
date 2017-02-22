f646a01ba4bf6fe9ec13b998e7bca814397c4085
Utsav Deep <utsav.deep@ril.com>
2017-02-23 02:22:16 +0530

    changes for fetching additional data

--- src/main/java/com/jcs/sbs/common/QueryTemplates.java
+++ src/main/java/com/jcs/sbs/common/QueryTemplates.java
@@ -1,12 +1,12 @@
 package com.jcs.sbs.common;
 
 public class QueryTemplates {
-    public static final String ACCOUNT_SUMMARY = "select A.project_id as project_id,totalSize,totalVolumes,"
-            + " stdVolumesGB,ssdVolumesGB,stdVolumes,ssdVolumes,bootableVolumes,userVolumes,encryptedVolumes,plaintextVolumes,"
-            + " coalesce(Q.volumeLimit,(select qc.hard_limit from quota_classes qc where qc.resource='volumes' and qc.deleted=0)) as volumeLimit,"
-            + " coalesce(Q.gbLimit,(select qc.hard_limit from quota_classes qc where qc.resource='gigabytes' and qc.deleted=0)) as gbLimit,"
-            + " coalesce(Q.snapshotLimit,(select qc.hard_limit from quota_classes qc where qc.resource='snapshots' and qc.deleted=0)) as snapshotLimit,"
-            + " coalesce(Q.perVolumeGBLimit,(select qc.hard_limit from quota_classes qc where qc.resource='per_volume_gigabytes' and qc.deleted=0)) as perVolumeGBLimit,"
+    public static final String ACCOUNT_SUMMARY = "select A.project_id as project_id,totalSize,totalVolumes,stdVolumesGB,ssdVolumesGB,stdVolumes,ssdVolumes,"
+            + " bootableVolumes,userVolumes,encryptedVolumes,plaintextVolumes,volumeQuotaUsages,gbQuotaUsages,snapshotQuotaUsages,"
+            + " coalesce(Q.volumeLimit,(select qc.hard_limit from quota_classes qc where qc.resource='volumes' and qc.class_name='default' and qc.deleted=0)) as volumeLimit,"
+            + " coalesce(Q.gbLimit,(select qc.hard_limit from quota_classes qc where qc.resource='gigabytes' and qc.class_name='default'  and qc.deleted=0)) as gbLimit,"
+            + " coalesce(Q.snapshotLimit,(select qc.hard_limit from quota_classes qc where qc.resource='snapshots' and qc.class_name='default' and qc.deleted=0)) as snapshotLimit,"
+            + " coalesce(Q.perVolumeGBLimit,(select qc.hard_limit from quota_classes qc where qc.resource='per_volume_gigabytes' and qc.class_name='default' and qc.deleted=0)) as perVolumeGBLimit,"
             + " coalesce(B.totalSnapshots,0) as totalSnapshots from "
             + " (select v.project_id, sum(v.size) as totalSize, count(v.id) as totalVolumes,"
             + " sum(CASE WHEN v.volume_type_id = 1 OR v.volume_type_id IS NULL THEN v.size ELSE 0 END) as stdVolumesGB,"
@@ -20,17 +20,22 @@ public class QueryTemplates {
             + " sum(case when resource='gigabytes' then hard_limit end) as gbLimit,"
             + " sum(case when resource='backups' then hard_limit end) as snapshotLimit,"
             + " sum(case when resource='per_volume_gigabytes' then hard_limit end) as perVolumeGBLimit"
-            + " from quotas group by project_id) Q on A.project_id = Q.project_id left join"
+            + " from quotas where deleted=0 group by project_id) Q on A.project_id = Q.project_id left join"
             + " (select count(b.id) as totalSnapshots,b.project_id from backups b where b.deleted = 0 and b.project_id IS NOT NULL group by b.project_id)"
-            + " B on A.project_id = B.project_id where %s like ?1 ORDER BY %s %s limit %s, %s";
+            + " B on A.project_id = B.project_id left join"
+            + " (select project_id, sum(case when resource='volumes' then in_use end) as volumeQuotaUsages,"
+            + " sum(case when resource='gigabytes' then in_use end) as gbQuotaUsages,"
+            + " sum(case when resource='backups' then in_use end) as snapshotQuotaUsages"
+            + " from quota_usages where deleted=0 group by project_id) QU on A.project_id = QU.project_id "
+            + " where %s like ?1 ORDER BY %s %s limit %s, %s";
 
     public static final String ACCOUNT_SUMMARY_RESULTS = "select count(AC.project_id) as totalResults from "
             + "(select A.project_id as project_id,totalSize,totalVolumes,"
             + " stdVolumesGB,ssdVolumesGB,stdVolumes,ssdVolumes,bootableVolumes,userVolumes,encryptedVolumes,plaintextVolumes,"
-            + " coalesce(Q.volumeLimit,(select qc.hard_limit from quota_classes qc where qc.resource='volumes' and qc.deleted=0)) as volumeLimit,"
-            + " coalesce(Q.gbLimit,(select qc.hard_limit from quota_classes qc where qc.resource='gigabytes' and qc.deleted=0)) as gbLimit,"
-            + " coalesce(Q.snapshotLimit,(select qc.hard_limit from quota_classes qc where qc.resource='snapshots' and qc.deleted=0)) as snapshotLimit,"
-            + " coalesce(Q.perVolumeGBLimit,(select qc.hard_limit from quota_classes qc where qc.resource='per_volume_gigabytes' and qc.deleted=0)) as perVolumeGBLimit,"
+            + " coalesce(Q.volumeLimit,(select qc.hard_limit from quota_classes qc where qc.resource='volumes' and qc.class_name='default' and qc.deleted=0)) as volumeLimit,"
+            + " coalesce(Q.gbLimit,(select qc.hard_limit from quota_classes qc where qc.resource='gigabytes' and qc.class_name='default' and qc.deleted=0)) as gbLimit,"
+            + " coalesce(Q.snapshotLimit,(select qc.hard_limit from quota_classes qc where qc.resource='snapshots' and qc.class_name='default' and qc.deleted=0)) as snapshotLimit,"
+            + " coalesce(Q.perVolumeGBLimit,(select qc.hard_limit from quota_classes qc where qc.resource='per_volume_gigabytes' and qc.class_name='default' and qc.deleted=0)) as perVolumeGBLimit,"
             + " coalesce(B.totalSnapshots,0) as totalSnapshots from "
             + " (select v.project_id, sum(v.size) as totalSize, count(v.id) as totalVolumes,"
             + " sum(CASE WHEN v.volume_type_id = 1 OR v.volume_type_id IS NULL THEN v.size ELSE 0 END) as stdVolumesGB,"
@@ -44,9 +49,13 @@ public class QueryTemplates {
             + " sum(case when resource='gigabytes' then hard_limit end) as gbLimit,"
             + " sum(case when resource='backups' then hard_limit end) as snapshotLimit,"
             + " sum(case when resource='per_volume_gigabytes' then hard_limit end) as perVolumeGBLimit"
-            + " from quotas group by project_id) Q on A.project_id = Q.project_id left join"
+            + " from quotas where deleted=0 group by project_id) Q on A.project_id = Q.project_id left join"
             + " (select count(b.id) as totalSnapshots,b.project_id from backups b where b.deleted = 0 and b.project_id IS NOT NULL group by b.project_id)"
-            + " B on A.project_id = B.project_id where %s like ?1) AC";
+            + " B on A.project_id = B.project_id left join"
+            + " (select project_id, sum(case when resource='volumes' then in_use end) as volumeQuotaUsages,"
+            + " sum(case when resource='gigabytes' then in_use end) as gbQuotaUsages,"
+            + " sum(case when resource='backups' then in_use end) as snapshotQuotaUsages"
+            + " from quota_usages where deleted=0 group by project_id) QU on A.project_id = QU.project_id where %s like ?1) AC";
 
     public static final String VOLUMES = "select * from (select v.id,v.project_id,v.size,v.snapshot_id,v.status,v.bootable,v.encrypted,"
             + "(COALESCE((select vt.name from volume_types vt where vt.id=v.volume_type_id),'standard')) as volume_type,"
--- src/main/java/com/jcs/sbs/result/AccountSummaryResult.java
+++ src/main/java/com/jcs/sbs/result/AccountSummaryResult.java
@@ -9,27 +9,30 @@ public class AccountSummaryResult {
     @Id
     @Column(name="project_id")
     private String project_id;
-    private int totalSize;
-    private int totalVolumes;
-    private int stdVolumesGB;
-    private int ssdVolumesGB;
-    private int stdVolumes;
-    private int ssdVolumes;
-    private int encryptedVolumes;
-    private int bootableVolumes;
-    private int userVolumes;
-    private int plaintextVolumes;
-    private int totalSnapshots;
-    private int volumeLimit;
-    private int gbLimit;
-    private int snapshotLimit;
-    private int perVolumeGBLimit;
+    private Integer totalSize;
+    private Integer totalVolumes;
+    private Integer stdVolumesGB;
+    private Integer ssdVolumesGB;
+    private Integer stdVolumes;
+    private Integer ssdVolumes;
+    private Integer encryptedVolumes;
+    private Integer bootableVolumes;
+    private Integer userVolumes;
+    private Integer plaintextVolumes;
+    private Integer totalSnapshots;
+    private Integer volumeLimit;
+    private Integer gbLimit;
+    private Integer snapshotLimit;
+    private Integer perVolumeGBLimit;
+    private Integer volumeQuotaUsages;
+    private Integer gbQuotaUsages;
+    private Integer snapshotQuotaUsages;
 
-    public int getPerVolumeGBLimit() {
+    public Integer getPerVolumeGBLimit() {
         return perVolumeGBLimit;
     }
 
-    public void setPerVolumeGBLimit(int perVolumeGBLimit) {
+    public void setPerVolumeGBLimit(Integer perVolumeGBLimit) {
         this.perVolumeGBLimit = perVolumeGBLimit;
     }
 
@@ -37,10 +40,11 @@ public class AccountSummaryResult {
         super();
     }
 
-    public AccountSummaryResult(String project_id, int totalSize, int totalVolumes, int stdVolumesGB, int ssdVolumesGB,
-            int stdVolumes, int ssdVolumes, int encryptedVolumes, int bootableVolumes, int userVolumes,
-            int plaintextVolumes, int totalSnapshots, int volumeLimit, int gbLimit, int snapshotLimit,
-            int perVolumeGBLimit) {
+
+    public AccountSummaryResult(String project_id, Integer totalSize, Integer totalVolumes, Integer stdVolumesGB, Integer ssdVolumesGB,
+            Integer stdVolumes, Integer ssdVolumes, Integer encryptedVolumes, Integer bootableVolumes, Integer userVolumes,
+            Integer plaintextVolumes, Integer totalSnapshots, Integer volumeLimit, Integer gbLimit, Integer snapshotLimit,
+            Integer perVolumeGBLimit, Integer volumeQuotaUsages, Integer gbQuotaUsages, Integer snapshotQuotaUsages) {
         super();
         this.project_id = project_id;
         this.totalSize = totalSize;
@@ -58,6 +62,9 @@ public class AccountSummaryResult {
         this.gbLimit = gbLimit;
         this.snapshotLimit = snapshotLimit;
         this.perVolumeGBLimit = perVolumeGBLimit;
+        this.volumeQuotaUsages = volumeQuotaUsages;
+        this.gbQuotaUsages = gbQuotaUsages;
+        this.snapshotQuotaUsages = snapshotQuotaUsages;
     }
 
     public AccountSummaryResult(AccountSummaryResult that) {
@@ -78,6 +85,9 @@ public class AccountSummaryResult {
         this.snapshotLimit = that.snapshotLimit;
         this.perVolumeGBLimit = that.perVolumeGBLimit;
         this.userVolumes = that.userVolumes;
+        this.volumeQuotaUsages = that.volumeQuotaUsages;
+        this.gbQuotaUsages = that.gbQuotaUsages;
+        this.snapshotQuotaUsages = that.snapshotQuotaUsages;
     }
 
     public String getProject_id() {
@@ -88,116 +98,140 @@ public class AccountSummaryResult {
         this.project_id = project_id;
     }
 
-    public int getTotalSize() {
+    public Integer getTotalSize() {
         return totalSize;
     }
 
-    public void setTotalSize(int totalSize) {
+    public void setTotalSize(Integer totalSize) {
         this.totalSize = totalSize;
     }
 
-    public int getTotalVolumes() {
+    public Integer getTotalVolumes() {
         return totalVolumes;
     }
 
-    public void setTotalVolumes(int totalVolumes) {
+    public void setTotalVolumes(Integer totalVolumes) {
         this.totalVolumes = totalVolumes;
     }
 
-    public int getStdVolumesGB() {
+    public Integer getStdVolumesGB() {
         return stdVolumesGB;
     }
 
-    public void setStdVolumesGB(int stdVolumesGB) {
+    public void setStdVolumesGB(Integer stdVolumesGB) {
         this.stdVolumesGB = stdVolumesGB;
     }
 
-    public int getSsdVolumesGB() {
+    public Integer getSsdVolumesGB() {
         return ssdVolumesGB;
     }
 
-    public void setSsdVolumesGB(int ssdVolumesGB) {
+    public void setSsdVolumesGB(Integer ssdVolumesGB) {
         this.ssdVolumesGB = ssdVolumesGB;
     }
 
-    public int getStdVolumes() {
+    public Integer getStdVolumes() {
         return stdVolumes;
     }
 
-    public void setStdVolumes(int stdVolumes) {
+    public void setStdVolumes(Integer stdVolumes) {
         this.stdVolumes = stdVolumes;
     }
 
-    public int getSsdVolumes() {
+    public Integer getSsdVolumes() {
         return ssdVolumes;
     }
 
-    public void setSsdVolumes(int ssdVolumes) {
+    public void setSsdVolumes(Integer ssdVolumes) {
         this.ssdVolumes = ssdVolumes;
     }
 
-    public int getEncryptedVolumes() {
+    public Integer getEncryptedVolumes() {
         return encryptedVolumes;
     }
 
-    public void setEncryptedVolumes(int encryptedVolumes) {
+    public void setEncryptedVolumes(Integer encryptedVolumes) {
         this.encryptedVolumes = encryptedVolumes;
     }
 
-    public int getBootableVolumes() {
+    public Integer getBootableVolumes() {
         return bootableVolumes;
     }
 
-    public void setBootableVolumes(int bootableVolumes) {
+    public void setBootableVolumes(Integer bootableVolumes) {
         this.bootableVolumes = bootableVolumes;
     }
 
-    public int getPlaintextVolumes() {
+    public Integer getPlaintextVolumes() {
         return plaintextVolumes;
     }
 
-    public void setPlaintextVolumes(int plaintextVolumes) {
+    public void setPlaintextVolumes(Integer plaintextVolumes) {
         this.plaintextVolumes = plaintextVolumes;
     }
 
-    public int getTotalSnapshots() {
+    public Integer getTotalSnapshots() {
         return totalSnapshots;
     }
 
-    public void setTotalSnapshots(int totalSnapshots) {
+    public void setTotalSnapshots(Integer totalSnapshots) {
         this.totalSnapshots = totalSnapshots;
     }
 
-    public int getVolumeLimit() {
+    public Integer getVolumeLimit() {
         return volumeLimit;
     }
 
-    public void setVolumeLimit(int volumeLimit) {
+    public void setVolumeLimit(Integer volumeLimit) {
         this.volumeLimit = volumeLimit;
     }
 
-    public int getGbLimit() {
+    public Integer getGbLimit() {
         return gbLimit;
     }
 
-    public void setGbLimit(int gbLimit) {
+    public void setGbLimit(Integer gbLimit) {
         this.gbLimit = gbLimit;
     }
 
-    public int getSnapshotLimit() {
+    public Integer getSnapshotLimit() {
         return snapshotLimit;
     }
 
-    public void setSnapshotLimit(int snapshotLimit) {
+    public void setSnapshotLimit(Integer snapshotLimit) {
         this.snapshotLimit = snapshotLimit;
     }
 
-    public int getUserVolumes() {
+    public Integer getUserVolumes() {
         return userVolumes;
     }
 
-    public void setUserVolumes(int userVolumes) {
+    public void setUserVolumes(Integer userVolumes) {
         this.userVolumes = userVolumes;
+    }
+
+    public Integer getVolumeQuotaUsages() {
+        return volumeQuotaUsages;
+    }
+
+    public void setVolumeQuotaUsages(Integer volumeQuotaUsages) {
+        this.volumeQuotaUsages = volumeQuotaUsages;
+    }
+
+    public Integer getGbQuotaUsages() {
+        return gbQuotaUsages;
+    }
+
+    public void setGbQuotaUsages(Integer gbQuotaUsages) {
+        this.gbQuotaUsages = gbQuotaUsages;
+    }
+
+    public Integer getSnapshotQuotaUsages() {
+        return snapshotQuotaUsages;
+    }
+
+    public void setSnapshotQuotaUsages(Integer snapshotQuotaUsages) {
+        this.snapshotQuotaUsages = snapshotQuotaUsages;
     }
 
     @Override
@@ -208,7 +242,9 @@ public class AccountSummaryResult {
                 + ", bootableVolumes=" + bootableVolumes + ", userVolumes=" + userVolumes + ", plaintextVolumes="
                 + plaintextVolumes + ", totalSnapshots=" + totalSnapshots + ", volumeLimit=" + volumeLimit
                 + ", gbLimit=" + gbLimit + ", snapshotLimit=" + snapshotLimit + ", perVolumeGBLimit=" + perVolumeGBLimit
-                + "]";
+                + ", volumeQuotaUsages=" + volumeQuotaUsages + ", gbQuotaUsages=" + gbQuotaUsages
+                + ", snapshotQuotaUsages=" + snapshotQuotaUsages + "]";
     }
+
 
 }
